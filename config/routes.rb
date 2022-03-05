ffa81e99252f403ee8d0df1962ea5d19c0770eec
rene3255 <ranchoeltecal.com>
2022-03-05 16:41:07 -0600

    Devise implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :supplies
   resources :breeds
+
+  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   #get 'home/index'
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root to: "home#index"
 end
